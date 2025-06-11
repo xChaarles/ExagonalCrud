f3136131c717e2c9af39a0276482747408c98040
xchaarles <carlosbp2608@gmail.com>
2025-06-11 01:17:31 -0500

    Changes AsignedCourseStudent

--- src/main/java/com/exaginalCrud/demo/infrastructure/adapters/input/rest/model/request/StudentCourseAssignRequest.java
+++ src/main/java/com/exaginalCrud/demo/infrastructure/adapters/input/rest/model/request/StudentCourseAssignRequest.java
@@ -1,18 +1,21 @@
 package com.exaginalCrud.demo.infrastructure.adapters.input.rest.model.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-@Getter
-@Setter
-@Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class StudentCourseAssignRequest {
 
     @NotNull(message = "Student ID cannot be null")
+    @JsonProperty("studentId")
     private Long studentId;
 
     @NotNull(message = "Course ID cannot be null")
+    @JsonProperty("courseId")
     private Long courseId;
+
 }
